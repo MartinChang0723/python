43da2cbfb46db0c9a17965241d664fc275e03b00
Martin <martin@intrising.com>
2018-03-18 14:22:06 +0800

    ncnc csie

---
+++ ncnuCsieFaculty.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.csie.ncnu.edu.tw/Default.aspx?CID=17')
+
+# print ("%s" % r.text)
+
+r.encoding = "UTF-8"
+
+s = BeautifulSoup(r.text, "html5lib")
+
+# print (s)
+
+tables = s.find_all("td", {"valign": "middle"})
+
+# print(tables)
+
+for t in tables:
+  if len(t.find("font").find_all("a")) < 2:
+    name = t.find("font").contents[0]
+    mail = t.find("font").contents[7].string
+    name.encoding = 'UTF-8'
+    mail.encoding = 'UTF-8'
+
+    print ("%s %s" % (name, mail))
+  else:
+    name = t.find("font").find_all("a")[0].string
+    name.encoding = 'UTF-8'
+
+    mail = t.find("font").find_all("a")[1].string
+    mail.encoding = 'UTF-8'
+
+    print ("%s %s" % (name, mail))
