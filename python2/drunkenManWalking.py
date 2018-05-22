4ad9b677a930c45d6ac0eb7508b154400855fb14
Martin.ccf <martin@intrising.com.tw>
2018-05-22 12:44:06 +0800

    hw

---
+++ python2/drunkenManWalking.py
@@ -0,0 +1,37 @@
+from random import randint
+
+p = ''
+pN = 0
+while True:
+  p = input("Input the initial position (p) -- ")
+  pN = int(p)
+  if pN >= 10:
+    print('input too big, only 1~9 is valid.')
+  elif pN <= 0:
+    print('input too small, only 1~9 is valid.')
+  else:
+    break
+
+def printArr (arr) :
+  myStr = ''
+  for i in arr:
+    myStr = myStr + str(i)
+  print (myStr)
+
+def simOneIteration (n) :
+  road = ['|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|']
+  road[n] = '*'
+  printArr(road)
+    
+while (10 > pN > 0):
+  simOneIteration(pN)
+  direction = randint(0, 1)
+  if direction == 0:
+    pN = pN + 1
+  else:
+    pN = pN - 1
+
+if (pN == 10):
+  print ('He falls into a river in a cold winter!')
+elif (pN == 0):
+  print ('He arrives home safely.')
